3f7cea2692942a17396b27b6eee21dac8661e522
dh0728 <dustkscjswo@naver.com>
2024-11-18 12:48:26 +0900

    feat: 로그인 기능 추가 회원가입, 로그인 기능 추가하는중

--- back/backend/settings.py
+++ back/backend/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'drf_spectacular',
+
     'accounts', #유저 앱
     
     # DRF
@@ -56,10 +58,20 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ]
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
 }
 
 # social login 
