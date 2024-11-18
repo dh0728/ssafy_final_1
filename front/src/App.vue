f97cf48a4d775182d13fb5a6b851794ddf4712ec
zeeeyeon <zeeyeonleee@gmail.com>
2024-11-18 11:51:09 +0900

    feat: registration

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
+    <Navbar />
 
+    <RouterView />
   </div>
 </template>
 
 <script setup>
 
+import Navbar from "@/components/common/Navbar.vue";
+import {RouterView} from "vue-router";
 </script>
 
 <style scoped>
