3bf9a2b88208547082a38e3ea0b7b08774b13025
dh0728 <dustkscjswo@naver.com>
2024-11-19 14:53:21 +0900

    feat 로그인유지기능추가 로컬스토리지에 토큰추가 로컬스토리지 토근으로 상태관리

--- back/accounts/serializers.py
+++ back/accounts/serializers.py
@@ -1,4 +1,5 @@
 from dj_rest_auth.registration.serializers import RegisterSerializer
+from dj_rest_auth.serializers import UserDetailsSerializer
 from rest_framework import serializers
 
 class UserRegisterSerializer(RegisterSerializer):
@@ -13,4 +14,11 @@ class UserRegisterSerializer(RegisterSerializer):
         data['birth'] = self.validated_data.get('birth')
         return data
 
+# 나중에 하기
+# class CustomUserDetailsSerializer(UserDetailsSerializer):
+#     # 기존 필드 이외에 추가하고 싶은 필드를 정의합니다.
+#     birth = serializers.DateField(read_only=True)
+#     username = serializers.CharField(read_only=True)
 
+#     class Meta(UserDetailsSerializer.Meta):
+#         fields = UserDetailsSerializer.Meta.fields + ('birth', 'username')
