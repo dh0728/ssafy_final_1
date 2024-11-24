106069125eaa26fc24d13fd01d4fb6c8d3a664e4
dh0728 <dustkscjswo@naver.com>
2024-11-25 01:20:50 +0900

    fixed:계정정보조회시 username과 email도뜨게 수정

--- back/backend/urls.py
+++ back/backend/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')), 
     path('accounts/', include('dj_rest_auth.urls')),
     path('accounts/registration/',include('dj_rest_auth.registration.urls')),
 
